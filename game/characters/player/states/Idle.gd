f07adcda556a290ad3943c43597cae4ee800808e
Joshua Letcher <caffeinatedcm@gmail.com>
2020-07-25 18:15:28 -0400

    Use player model

--- game/characters/player/states/Idle.gd
+++ game/characters/player/states/Idle.gd
@@ -1,2 +1,13 @@
-extends State
+extends MovementBaseState
 
+func enter():
+    animationState.travel("Idle")
+
+func update(_delta):
+    if Input.is_action_just_pressed("jump"):
+        emit_signal("finished", "jump")
+    if abs(Input.get_action_strength("move_right") - Input.get_action_strength("move_left")) > 0:
+        emit_signal("finished", "moving")
+    
+    velocity = detect_player_velocity(0)
+    velocity = player.move_and_slide(velocity)
--- game/characters/player/states/PlayerBaseState.gd
+++ game/characters/player/states/PlayerBaseState.gd
@@ -2,4 +2,6 @@ extends State
 class_name PlayerBaseState
 
 onready var player = get_parent().get_parent()
-
+onready var animationTree = player.get_node("playerchar/AnimationTree")
+onready var animationState = animationTree.get("parameters/playback")
+onready var playerMesh = player.get_node("playerchar")
