d915f8a4ef8cd870cd55bd944133f0905445980d
Joshua Letcher <caffeinatedcm@gmail.com>
2020-07-25 20:48:36 -0400

    Make flying enemy attack

--- game/characters/FlyingBasicEnemy/FlyingBasicEnemy.gd
+++ game/characters/FlyingBasicEnemy/FlyingBasicEnemy.gd
@@ -1,4 +1,12 @@
 extends KinematicBody
 
+onready var stats = $Stats
+
+func _ready():
+    stats.connect("on_no_health", self, "queue_free")
+
+func take_damage(value):
+    stats.health -= value
+
 func _on_Hurtbox_body_entered(player):
     player.take_damage($Stats.DAMAGE)
--- game/characters/FlyingBasicEnemy/States/Attacking.gd
+++ game/characters/FlyingBasicEnemy/States/Attacking.gd
@@ -1,14 +1,20 @@
 extends FlyingBasicEnemyBaseState
 
+export var MAX_ATTACK_SPEED = 20
+
 var target
+var velocity
 
 func enter():
     target = playerDetector.get_overlapping_bodies()[0]
     playerDetector.connect("body_exited", self, "_on_player_left")
+    velocity = Vector3.ZERO
     
 func exit():
     playerDetector.disconnect("body_exited", self, "_on_player_left")
 
 func update(delta):
-    # TODO: Attack the player
-    pass
+    var direction = enemy.global_transform.origin.direction_to(target.global_transform.origin)
+    
+    velocity = velocity.linear_interpolate(direction * MAX_ATTACK_SPEED, delta)
+    velocity = enemy.move_and_slide(velocity, Vector3.UP)
