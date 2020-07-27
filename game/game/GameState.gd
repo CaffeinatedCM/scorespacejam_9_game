81dee7f8a68ee96c279b24380b7490eb47c57082
Joshua Letcher <caffeinatedcm@gmail.com>
2020-07-27 01:12:52 -0400

    Start working on boss

--- game/game/GameState.gd
+++ game/game/GameState.gd
@@ -6,20 +6,20 @@ var totalKills = 0
 signal total_kills_updated()
 
 func _ready():
-	SilentWolf.configure({
-		"api_key": "dm4pUZRlg11BHMSWWeVIlOiLATbYLc47gKeQ9Zmc",
-		"game_id": "gamejam",
-		"game_version": "1.0.0",
-		"log_level": 1
-	  })
+    SilentWolf.configure({
+        "api_key": "dm4pUZRlg11BHMSWWeVIlOiLATbYLc47gKeQ9Zmc",
+        "game_id": "gamejam",
+        "game_version": "1.0.0",
+        "log_level": 1
+      })
 
-	#   SilentWolf.configure_scores({
-	# 	"open_scene_on_close": "res://scenes/MainPage.tscn"
-	#   })
+    #   SilentWolf.configure_scores({
+    # 	"open_scene_on_close": "res://scenes/MainPage.tscn"
+    #   })
 
 func increase_total_kills():
-	totalKills += 1
-	emit_signal("total_kills_updated")
+    totalKills += 1
+    emit_signal("total_kills_updated")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
